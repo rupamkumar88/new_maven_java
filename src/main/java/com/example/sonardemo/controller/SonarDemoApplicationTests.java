8c5793ddd7e11fe9abe6858fc9d86a4c8592b81f
rupamkumar88 <rupamdandge88.gmail.com>
2024-09-01 03:36:51 -0400

    csd

---
+++ src/main/java/com/example/sonardemo/controller/SonarDemoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.sonardemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
